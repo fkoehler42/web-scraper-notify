3da1f486a1c0f0b442533522296cea808e690b0e
Flavien Koehler <flavien@willo.com>
2022-05-10 11:26:30 +0200

    Remove template package data

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,3 @@
-export interface Toppings {
-    peppers: boolean
-    pineapple: boolean
-    bbqSauce: boolean
-    cheeseType: string
-}
+const main = () => null;
 
-export async function orderPizza (toppings: Toppings): Promise<{ message: string}> {
-    let message = 'you ordered a pizza with:\n';
-    if (toppings.peppers) message += '  - peppers\n';
-    if (toppings.pineapple) message += '  - pineapple\n';
-    if (toppings.bbqSauce) message += '  - bbq\n';
-    message += `  - ${toppings.cheeseType} cheese`;
-    return {
-        message
-    };
-}
+export default main;
